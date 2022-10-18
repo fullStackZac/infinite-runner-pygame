1f141aec6993215dcb68634cf2d9b5e0af77534e
fullStackZac <zac.childress.sr@gmail.com>
2022-10-18 10:42:45 -0500

    learning pygame

---
+++ game.py
@@ -0,0 +1,31 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption('Chloe & The Lantern Empire')
+clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+
+sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
+text_surface = test_font.render('New Font', False, 'Black')
+
+snail_surface = pygame.image.load('graphics/snail/snail1.png')
+snail_x_pos = 600
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    
+    screen.blit(sky_surface, (0,0))
+    screen.blit(ground_surface, (0,300))
+    screen.blit(text_surface,(300,50))
+    snail_x_pos -= 4
+    if snail_x_pos < -100: snail_x_pos = 800
+    screen.blit(snail_surface,(snail_x_pos,250))
+   
+    pygame.display.update()
+    clock.tick(60)
